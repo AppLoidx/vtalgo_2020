a938adcb1e3c568f73c0d3eb9f9e635f97d07198
Arthur Kupriyanov <apploidyakutsk@gmail.com>
2020-02-07 18:31:43 +0300

    Create 2025.c

---
+++ intro/2025.c
@@ -0,0 +1,20 @@
+//gcc 5.4.0
+
+#include  <stdio.h>
+
+int main()
+{
+    int iter, n,k;
+    scanf("%d", &iter);
+    
+    for (int i = 0; i < iter; i++){   
+        scanf("%d%d", &n, &k);
+        int diff = n % k;  
+        int each = n / k;  
+        int a = ( n - each ) * (k - diff) * each; 
+        int b = ( n - (each + 1)) * diff * (each + 1);  
+
+        int res = (a + b)  / 2;
+        printf("%d\n", res);
+    }
+}
