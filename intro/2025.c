a820567117ddf30afce2d10878f45bde9171c1ba
Arthur Kupriyanov <apploidyakutsk@gmail.com>
2020-02-11 00:34:34 +0300

    LLVM format

--- intro/2025.c
+++ intro/2025.c
@@ -1,20 +1,25 @@
-//gcc 5.4.0
 
-#include  <stdio.h>
+/*****************************************************
+ * Task 2025                                         *
+ *                                                   *
+ * TIMUS                                             *
+ *                                                   *
+ *****************************************************/
 
-int main()
-{
-    int iter, n,k;
-    scanf("%d", &iter);
-    
-    for (int i = 0; i < iter; i++){   
-        scanf("%d%d", &n, &k);
-        int diff = n % k;  
-        int each = n / k;  
-        int a = ( n - each ) * (k - diff) * each; 
-        int b = ( n - (each + 1)) * diff * (each + 1);  
+#include <stdio.h>
 
-        int res = (a + b)  / 2;
-        printf("%d\n", res);
-    }
+int main() {
+  int iter, n, k;
+  scanf("%d", &iter);
+
+  for (int i = 0; i < iter; i++) {
+    scanf("%d%d", &n, &k);
+    int diff = n % k;
+    int each = n / k;
+    int a = (n - each) * (k - diff) * each;
+    int b = (n - (each + 1)) * diff * (each + 1);
+
+    int res = (a + b) / 2;
+    printf("%d\n", res);
+  }
 }
